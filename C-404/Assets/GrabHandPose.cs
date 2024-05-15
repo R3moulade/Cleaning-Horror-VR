448b5d9926e28c4d8ae02478228177ca57fe2db7
Dyrby <maddie.dyrby.hansen@outlook.dk>
2024-05-15 14:26:35 +0200

    rage quit

--- C-404/Assets/GrabHandPose.cs
+++ C-404/Assets/GrabHandPose.cs
@@ -6,48 +6,95 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class GrabHandPose : MonoBehaviour
 {
     public HandData rightHandPose;
+
     private Vector3 startingHandPosition;
     private Vector3 finalHandPosition;
     private Quaternion startingHandRotation;
     private Quaternion finalHandRotation;
+
     private Quaternion[] startingFingerRotations;
     private Quaternion[] finalFingerRotations;
+
+
+
     void Start()
     {
         XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+
         grabInteractable.selectEntered.AddListener(SetupPose);
+        grabInteractable.selectExited.AddListener(UnSetPose);
+
         rightHandPose.gameObject.SetActive(false);
     }
 
-    public void SetupPose(BaseInteractionEventArgs arg)
+public void SetupPose(BaseInteractionEventArgs arg)
+{
+    if(arg.interactorObject is XRDirectInteractor)
     {
-        if(arg.interactorObject is XRDirectInteractor)
+        if(arg.interactorObject.transform != null)
         {
             HandData handData = arg.interactorObject.transform.GetComponentInChildren<HandData>();
-            handData.animator.enabled = false;
+            if(handData != null)
+            {
+                handData.animator.enabled = false;
 
-            SetHandDataValues(handData, rightHandPose);
-            SetHandData(handData, finalHandPosition, finalHandRotation, finalFingerRotations);
+                SetHandDataValues(handData, rightHandPose);
+                SendHandData(handData, finalHandPosition, finalHandRotation, finalFingerRotations);
+            }
+            else
+            {
+                Debug.LogError("HandData component not found in the children of the interactor object." + arg.interactorObject.transform.name);
+            }
+        }
+        else
+        {
+            Debug.LogError("Interactor object's transform is null.");
         }
     }
+}
+public void UnSetPose(BaseInteractionEventArgs arg) 
+{
+    if (arg.interactorObject is XRDirectInteractor)
+    {
+        if(arg.interactorObject.transform != null)
+        {
+            HandData handData = arg.interactorObject.transform.GetComponentInChildren<HandData>();
+            if(handData != null)
+            {
+                handData.animator.enabled = true;
+                SendHandData(handData, startingHandPosition, startingHandRotation, startingFingerRotations);
+            }
+            else
+            {
+                Debug.LogError("HandData component not found in the children of the interactor object.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Interactor object's transform is null.");
+        }
+    }
+}
     public void SetHandDataValues(HandData h1, HandData h2)
     {
-        startingHandPosition = h1.root.localPosition;
-        finalHandPosition = h2.root.localPosition;
-
+        startingHandPosition = new Vector3(h1.root.localPosition.x / h1.root.localScale.x,
+            h1.root.localPosition.y / h1.root.localScale.y, h1.root.localPosition.z / h1.root.localScale.z);
+        finalHandPosition = new Vector3(h2.root.localPosition.x / h2.root.localScale.x,
+            h2.root.localPosition.y / h2.root.localScale.y, h2.root.localPosition.z / h2.root.localScale.z);
+       
         startingHandRotation = h1.root.localRotation;
         finalHandRotation = h2.root.localRotation;
 
         startingFingerRotations = new Quaternion[h1.fingerBones.Length];
         finalFingerRotations = new Quaternion[h1.fingerBones.Length];
 
-        for (int i = 0; i < h1.fingerBones.Length; i++)
+            for (int i = 0; i < h1.fingerBones.Length; i++)
         {
             startingFingerRotations[i] = h1.fingerBones[i].localRotation;
             finalFingerRotations[i] = h2.fingerBones[i].localRotation;
         }
     }
-    public void SetHandData(HandData h, Vector3 newPosition, Quaternion newRotation, Quaternion[] newBonesRotation)
+    public void SendHandData(HandData h, Vector3 newPosition, Quaternion newRotation, Quaternion[] newBonesRotation)
     {
         h.root.localPosition = newPosition;
         h.root.localRotation = newRotation;
